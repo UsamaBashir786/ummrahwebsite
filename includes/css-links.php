e5c59468ce7e6faffee9c6ba6e859d9adeacd950
Usama Bashir <usamab@mudsoft.tech>
2025-05-02 10:10:38 +0500

    your message

--- includes/css-links.php
+++ includes/css-links.php
@@ -4,4 +4,4 @@
 <link rel="stylesheet" href="assets/css/style.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/validate.js/0.13.1/validate.min.js"></script>
 <link href="src/output.css" rel="stylesheet">
-
+<link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
